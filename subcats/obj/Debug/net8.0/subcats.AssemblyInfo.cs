5a2f0c890f9fa9ebcbcc422b02954aa111623241
KARINA LIZETH ALVAREZ ANDINO <klalvarez@uth.hn>
2025-03-27 10:49:00 -0600

    seagregovalidacionenstock

--- subcats/obj/Debug/net8.0/subcats.AssemblyInfo.cs
+++ subcats/obj/Debug/net8.0/subcats.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("subcats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dacd2212d27dd4216053c04f84f6db1c84f6a7c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0be160e0025e0922323a7bbb9064bf3758cf1fc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("subcats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("subcats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
